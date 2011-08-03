a7960c65b2a4acb5b5a60af44eecd83aec08f921
William Panting <willtp87@hotmail.com>
2011-08-03 10:26:20 -0300

    forgot to add re_alias_jQuery in last commit

---
+++ jQuery/re_alias_jQuery.js
@@ -0,0 +1,12 @@
+/**
+ * @file
+ *   This file is used to allow access to a more current version of jQuery then is in D6.
+ *   Under the alias ir_citation_jQuery
+ * @author
+ *   William Panting
+*/
+
+var ir_citation_jQuery = jQuery.noConflict(true);
+
+
+
