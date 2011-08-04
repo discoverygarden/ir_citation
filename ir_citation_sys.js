7303f9413b0b12dd9789972f2bd956bbac16e265
William Panting <willtp87@hotmail.com>
2011-08-04 10:06:35 -0300

    better docs

--- ir_citation_sys.js
+++ ir_citation_sys.js
@@ -3,27 +3,36 @@
  *   This javascript creates a sys object for the citeproc-js engine.  This object is meant to allow 
  *   access to abreviations, citation data and localization information that has been made available
  *   through the module's API.
- * @todo
- *   This needs to be modified to work with stuff that gets set up through drupal
  * @author 
  *   William Panting
- * @param abbreviations
- * @returns nada
- *   nill
  */
 
+/**
+ * Constructor
+ */
 var ir_citation_sys = function(){
 	return;
 };
 
+/**
+ * This function will let the citeproc-js engine access to citation data set by the Drupal module
+ * 
+ */
 ir_citation_sys.prototype.retrieveItem = function(id){
 	return ir_citation_jQuery.parseJSON(Drupal.settings.ir_citation.citeproc_json[id]);
 };
 
+/**
+ * This function will let the citeproc-js engine access to localization data set by the Drupal module
+ * 
+ */
 ir_citation_sys.prototype.retrieveLocale = function(lang){
 	return Drupal.settings.ir_citation.locale[lang];
 };
 
+/**
+ * This function will let the citeproc-js engine access to abbreviation data set by the Drupal module
+ */
 ir_citation_sys.prototype.getAbbreviations = function(name, vartype){
 	return ir_citation_jQuery.parseJSON(Drupal.settings.ir_citation.abbreviations[name][vartype]);
 };
