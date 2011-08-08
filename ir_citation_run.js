0b011504e1ad7ea6ce0f6532f7ad33a9f6d6c612
William Panting <willtp87@hotmail.com>
2011-08-08 16:31:22 -0300

    citation objects being sent from drupal now

--- ir_citation_run.js
+++ ir_citation_run.js
@@ -35,293 +35,6 @@ CiteInserter.prototype.insertCite = function (cites) {
 	this.idx += 1;
 }
 
-var citationCAD1 = {
-	"citationItems": [
-        {
-			id: "ITEM-1"
-		}
-	],
-	"properties": {
-		"noteIndex": 1
-	}
-};
-
-var citationCAD2 = {
-	"citationItems": [
-        {
-			id: "ITEM-2"
-		}
-	],
-	"properties": {
-		"noteIndex": 2
-	}
-};
-
-var citationCAD3 = {
-	"citationItems": [
-        {
-			id: "ITEM-3"
-		}
-	],
-	"properties": {
-		"noteIndex": 3
-	}
-};
-
-var citationCAD4 = {
-	"citationItems": [
-        {
-			id: "ITEM-4"
-		}
-	],
-	"properties": {
-		"noteIndex": 4
-	}
-};
-
-var citationCAD5 = {
-	"citationItems": [
-        {
-			id: "ITEM-5"
-		}
-	],
-	"properties": {
-		"noteIndex": 5
-	}
-};
-
-var citationCAD6 = {
-	"citationItems": [
-        {
-			id: "ITEM-6"
-		}
-	],
-	"properties": {
-		"noteIndex": 6
-	}
-};
-
-var citationCAD7 = {
-	"citationItems": [
-        {
-			id: "ITEM-21"
-		}
-	],
-	"properties": {
-		"noteIndex": 7
-	}
-};
-
-var citationBB1 = {
-	"citationItems": [
-        {
-			id: "ITEM-6",
-			label: "page",
-			locator: "223"
-		}
-	],
-	"properties": {
-		"noteIndex": 1
-	}
-};
-
-var citationBB2 = {
-	"citationItems": [
-        {
-			id: "ITEM-6"
-		},
-        {
-			id: "ITEM-10",
-			label: "page",
-			locator: "685"
-		},
-        {
-			id: "ITEM-11",
-			label: "page",
-			locator: "388"
-		},
-        {
-			id: "ITEM-12",
-			label: "page",
-			locator: "359"
-		},
-        {
-			id: "ITEM-10",
-			label: "page",
-			locator: "690"
-		},
-        {
-			id: "ITEM-11",
-			label: "page",
-			locator: "393"
-		},
-        {
-			id: "ITEM-12",
-			label: "page",
-			locator: "364"
-		},
-        {
-			id: "ITEM-6",
-			locator: "15",
-			prefix:"<i>but see</i>"
-		}
-	],
-	"properties": {
-		"noteIndex": 2
-	}
-};
-
-var citationBB3 = {
-	"citationItems": [
-        {
-			id: "ITEM-13",
-			locator:"90",
-			label:"section"
-		},
-        {
-			id: "ITEM-14",
-			locator:"7",
-			label:"section"
-		},
-        {
-			id: "ITEM-15",
-			locator:"731-32",
-			label:"page"
-		},
-        {
-			id: "ITEM-16"
-		}
-	],
-	"properties": {
-		"noteIndex": 3
-	}
-};
-
-var citationBB4 = {
-	"citationItems": [
-        {
-			id: "ITEM-2"
-		}
-	],
-	"properties": {
-		"noteIndex": 4
-	}
-};
-
-var citationBB5 = {
-	"citationItems": [
-        {
-			id: "ITEM-2",
-			label:"page",
-			locator:"482"
-		},
-        {
-			id: "ITEM-13",
-			locator:"395",
-			label:"section"
-		},
-        {
-			id: "ITEM-1",
-			label:"page",
-			locator:"25"
-		},
-        {
-			id: "ITEM-1"
-		},
-        {
-			id: "ITEM-1",
-			label:"page",
-			locator:"112"
-		}
-	],
-	"properties": {
-		"noteIndex": 5
-	}
-};
-
-var citationBB6 = {
-	"citationItems": [
-        {
-			id: "ITEM-16"
-		}
-	],
-	"properties": {
-		"noteIndex": 6
-	}
-};
-
-var citationBB7 = {
-	"citationItems": [
-        {
-			id: "ITEM-17",
-			label:"paragraph",
-			locator:"6"
-		},
-        {
-			id: "ITEM-18",
-			label:"page",
-			locator:"983"
-		}
-	],
-	"properties": {
-		"noteIndex": 7
-	}
-};
-
-var citationBB8 = {
-	"citationItems": [
-        {
-			id: "ITEM-19",
-			"suffix":"(appeal taken from Scot.)"
-		}
-	],
-	"properties": {
-		"noteIndex": 8
-	}
-};
-
-var citationBB9 = {
-	"citationItems": [
-        {
-			id: "ITEM-20",
-			suffix:"(appeal taken from B.C.)"
-		}
-	],
-	"properties": {
-		"noteIndex": 9
-	}
-};
-
-var citationBB10 = {
-	"citationItems": [
-        {
-			id: "ITEM-3",
-			prefix:"<b>En sinn Scholl beschéngt ass, da Mamm frësch blénken hun?</b> <i>See, e.g.</i>"
-		},
-        {
-			prefix:"<b>Der mä gutt Dach, eng onser bléit geplot mä.</b>  <i>See generally</i>",
-			id: "ITEM-3",
-			suffix:"<b>(Iwer Engel Milliounen nei fu, blëtzen néierens d'Gaassen rou do.)</b>"
-		}
-	],
-	"properties": {
-		"noteIndex": 10
-	}
-};
-
-var citationBB11 = {
-	"citationItems": [
-        {
-			id: "ITEM-13",
-			label:"section",
-			locator:"1"
-		}
-	],
-	"properties": {
-		"noteIndex": 11
-	}
-};
-
 var insert = function(){
 	var citeproc, output;
 	
@@ -331,19 +44,19 @@ var insert = function(){
 	citeproc = new CSL.Engine(sys, Drupal.settings.ir_citation.csl.chicago_author_date);
 	var citeInserter = new CiteInserter("citation_cad","cad");
 	
-	var cad1 = citeproc.appendCitationCluster(citationCAD1);
+	var cad1 = citeproc.appendCitationCluster(ir_citation_jQuery.parseJSON(Drupal.settings.ir_citation.citation_objects['citationCAD1']));
 	citeInserter.insertCite(cad1);
-	var cad2 = citeproc.appendCitationCluster(citationCAD2);
+	var cad2 = citeproc.appendCitationCluster(ir_citation_jQuery.parseJSON(Drupal.settings.ir_citation.citation_objects['citationCAD2']));
 	citeInserter.insertCite(cad2);
-	var cad3 = citeproc.appendCitationCluster(citationCAD3);
+	var cad3 = citeproc.appendCitationCluster(ir_citation_jQuery.parseJSON(Drupal.settings.ir_citation.citation_objects['citationCAD3']));
 	citeInserter.insertCite(cad3);
-	var cad4 = citeproc.appendCitationCluster(citationCAD4);
+	var cad4 = citeproc.appendCitationCluster(ir_citation_jQuery.parseJSON(Drupal.settings.ir_citation.citation_objects['citationCAD4']));
 	citeInserter.insertCite(cad4);
-	var cad5 = citeproc.appendCitationCluster(citationCAD5);
+	var cad5 = citeproc.appendCitationCluster(ir_citation_jQuery.parseJSON(Drupal.settings.ir_citation.citation_objects['citationCAD5']));
 	citeInserter.insertCite(cad5);
-	var cad6 = citeproc.appendCitationCluster(citationCAD6);
+	var cad6 = citeproc.appendCitationCluster(ir_citation_jQuery.parseJSON(Drupal.settings.ir_citation.citation_objects['citationCAD6']));
 	citeInserter.insertCite(cad6);
-	var cad7 = citeproc.appendCitationCluster(citationCAD7);
+	var cad7 = citeproc.appendCitationCluster(ir_citation_jQuery.parseJSON(Drupal.settings.ir_citation.citation_objects['citationCAD7']));
 	//alert("Targeted update of cites\naffected by name addition:\n"+cad7);
 	citeInserter.insertCite(cad7);
 
@@ -352,27 +65,27 @@ var insert = function(){
 	citeInserter = new CiteInserter("citation", "citation");
 	citeproc.setAbbreviations("default");
 
-	var citation1 = citeproc.appendCitationCluster(citationBB1);
+	var citation1 = citeproc.appendCitationCluster(ir_citation_jQuery.parseJSON(Drupal.settings.ir_citation.citation_objects['citationBB1']));
 	citeInserter.insertCite(citation1);
-	var citation2 = citeproc.appendCitationCluster(citationBB2);
+	var citation2 = citeproc.appendCitationCluster(ir_citation_jQuery.parseJSON(Drupal.settings.ir_citation.citation_objects['citationBB2']));
 	citeInserter.insertCite(citation2);
-	var citation3 = citeproc.appendCitationCluster(citationBB3);
+	var citation3 = citeproc.appendCitationCluster(ir_citation_jQuery.parseJSON(Drupal.settings.ir_citation.citation_objects['citationBB3']));
 	citeInserter.insertCite(citation3);
-	var citation4 = citeproc.appendCitationCluster(citationBB4);
+	var citation4 = citeproc.appendCitationCluster(ir_citation_jQuery.parseJSON(Drupal.settings.ir_citation.citation_objects['citationBB4']));
 	citeInserter.insertCite(citation4);
-	var citation5 = citeproc.appendCitationCluster(citationBB5);
+	var citation5 = citeproc.appendCitationCluster(ir_citation_jQuery.parseJSON(Drupal.settings.ir_citation.citation_objects['citationBB5']));
 	citeInserter.insertCite(citation5);
-	var citation6 = citeproc.appendCitationCluster(citationBB6);
+	var citation6 = citeproc.appendCitationCluster(ir_citation_jQuery.parseJSON(Drupal.settings.ir_citation.citation_objects['citationBB6']));
 	citeInserter.insertCite(citation6);
-	var citation7 = citeproc.appendCitationCluster(citationBB7);
+	var citation7 = citeproc.appendCitationCluster(ir_citation_jQuery.parseJSON(Drupal.settings.ir_citation.citation_objects['citationBB7']));
 	citeInserter.insertCite(citation7);
-	var citation8 = citeproc.appendCitationCluster(citationBB8);
+	var citation8 = citeproc.appendCitationCluster(ir_citation_jQuery.parseJSON(Drupal.settings.ir_citation.citation_objects['citationBB8']));
 	citeInserter.insertCite(citation8);
-	var citation9 = citeproc.appendCitationCluster(citationBB9);
+	var citation9 = citeproc.appendCitationCluster(ir_citation_jQuery.parseJSON(Drupal.settings.ir_citation.citation_objects['citationBB9']));
 	citeInserter.insertCite(citation9);
-	var citation10 = citeproc.appendCitationCluster(citationBB10);
+	var citation10 = citeproc.appendCitationCluster(ir_citation_jQuery.parseJSON(Drupal.settings.ir_citation.citation_objects['citationBB10']));
 	citeInserter.insertCite(citation10);
-	var citation11 = citeproc.appendCitationCluster(citationBB11);
+	var citation11 = citeproc.appendCitationCluster(ir_citation_jQuery.parseJSON(Drupal.settings.ir_citation.citation_objects['citationBB11']));
 	citeInserter.insertCite(citation11);
 
 };
